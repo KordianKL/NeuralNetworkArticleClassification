947b7ad6a12b8dad2e800574b8757df06f8aaa60
Kordian Ledzion <kkordikk@gmail.com>
2017-06-05 12:54:09 +0200

    xXx

--- SIclass/Converter.swift
+++ SIclass/Converter.swift
@@ -10,16 +10,103 @@ import Foundation
 
 public class Converter {
     
-    var pathTrainingData : String
-    var pathTrainingLabels : String
-    var pathValidationData : String
-    var pathValidationLabels : String
+    public var trainData = [[Float]]()
+    public var trainLabels = [[Float]]()
+    public var testData = [[Float]]()
+    public var testLabels = [[Float]]()
     
-    public init(pathTrainingData : String, pathTrainingLabels : String, pathValidationData : String, pathValidationLabels : String){
-        self.pathTrainingData = pathTrainingData
-        self.pathTrainingLabels = pathTrainingLabels
-        self.pathValidationData = pathValidationData
-        self.pathValidationLabels = pathValidationLabels
+    public func run(){
+        
+        print("trainDATA START")
+        
+        //Wrapping trainDATA
+        
+        var placeholderArray = [[Float]](repeating: [Float](repeating: 0.0, count: 61188), count: 11269)
+        var placeholder = ""
+        var path = URL(string: "file:////Users/kordianledzion/Desktop/Kordian/Swift/SIclass/SIclass/trainDATA.txt")! // Your path here
+        do {
+            placeholder = try String(contentsOf: path, encoding: String.Encoding.utf8)
+        }
+        catch {print("ERROR reading file")}
+        placeholder.enumerateLines { (line, _) in
+            let split = line.components(separatedBy: " ")
+            placeholderArray[Int(split[0])!-1][Int(split[1])!-1] = Float(split[2])! / 100.0
+        }
+        
+        print("trainDATA DONE")
+        
+        trainData = placeholderArray
+        
+        
+        
+        
+        
+        print("trainLABEL START")
+        var index = 0
+        
+        //Wrapping trainLABEL
+        
+        placeholderArray = [[Float]](repeating: [Float](repeating: 0.0, count: 20), count: 11269)
+        path = URL(string: "file:////Users/kordianledzion/Desktop/Kordian/Swift/SIclass/SIclass/trainLABEL.txt")!
+        do {
+            placeholder = try String(contentsOf: path, encoding: String.Encoding.utf8)
+        }
+        catch {print("ERROR reading file")}
+        placeholder.enumerateLines { (line, _) in
+            placeholderArray[index][Int(line)!-1] = 1.0
+            index = index+1
+        }
+        
+        trainLabels = placeholderArray
+        index = 0
+        
+        print("trainLABEL DONE")
+        
+        
+        
+        
+        
+        print("testDATA START")
+        
+        //Wrapping testDATA
+        
+        placeholderArray = [[Float]](repeating: [Float](repeating: 0, count: 61188), count: 7505)
+        path = URL(string: "file:////Users/kordianledzion/Desktop/Kordian/Swift/SIclass/SIclass/testDATA.txt")!
+        do {
+            placeholder = try String(contentsOf: path, encoding: String.Encoding.utf8)
+        }
+        catch {print("ERROR reading file")}
+        placeholder.enumerateLines { (line, _) in
+            let split = line.components(separatedBy: " ")
+            placeholderArray[Int(split[0])!-1][Int(split[1])!-1] = Float(split[2])! / 100.0
+        }
+        
+        testData = placeholderArray
+        
+        print("testDATA DONE")
+        
+        
+        
+        
+        
+        print("testLABEL START")
+        
+        //Wrapping testLABEL
+        
+        placeholderArray = [[Float]](repeating: [Float](repeating: 0, count: 20), count: 7505)
+        path = URL(string: "file:////Users/kordianledzion/Desktop/Kordian/Swift/SIclass/SIclass/testLABEL.txt")!
+        do {
+            placeholder = try String(contentsOf: path, encoding: String.Encoding.utf8)
+        }
+        catch {print("ERROR reading file")}
+        placeholder.enumerateLines { (line, _) in
+            placeholderArray[index][Int(line)!-1] = 1.0
+            index = index+1
+        }
+        
+        testLabels = placeholderArray
+        
+        print("testLABEL DONE")
     }
     
 }
--- SIclass/main.swift
+++ SIclass/main.swift
@@ -8,5 +8,47 @@
 
 import Foundation
 
-print("Hello, World!")
+func getDocumentsDirectory() -> URL {
+    let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+    let documentsDirectory = paths[0]
+    return documentsDirectory
+}
 
+let conv = Converter()
+
+print("START")
+
+conv.run()
+
+print("Buduje strukture")
+
+let structure = try NeuralNet.Structure(nodes: [61188, 500, 20], hiddenActivation: .rectifiedLinear, outputActivation: .softmax)
+
+print("Tworze NN na podstawie struktury")
+
+let nn = try NeuralNet(structure: structure)
+
+print("Tworze dataset")
+
+let dataset = try NeuralNet.Dataset(trainInputs: conv.trainData, trainLabels: conv.trainLabels, validationInputs: conv.testData, validationLabels: conv.testLabels)
+
+print("Start uczenia")
+
+let (epochs, error) = try nn.train(dataset, maxEpochs: 50, errorThreshold: 0.02, errorFunction: .percentage) { (epoch, err) -> Bool in
+    
+    // Log progress
+    let percCorrect = (1 - err) * 100
+    let percError = err * 100
+    print("\nEpoch \(epoch)")
+    print("Accuracy:\t\(percCorrect)%")
+    print("Error:\t\t\(percError)%")
+    
+    // Decay learning rate and momentum
+    nn.learningRate *= 0.97
+    nn.momentumFactor *= 0.97
+    
+    // Allow training to continue
+    return true
+}
+
+print("FINISH")
